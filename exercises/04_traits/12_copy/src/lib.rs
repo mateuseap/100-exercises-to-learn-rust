96ef2322bc0e4f04b589cdab3ff7c81a87ecf48e
Mateus Elias <meap@cin.ufpe.br>
2024-11-04 05:55:09 -0300

    feat(04_traits/12_copy): add exercise solution

--- exercises/04_traits/12_copy/src/lib.rs
+++ exercises/04_traits/12_copy/src/lib.rs
@@ -1,6 +1,8 @@
 // TODO: implement the necessary traits to make the test compile and pass.
 //  You *can't* modify the test.
+use std::ops::Add;
 
+#[derive(PartialEq, Debug, Clone, Copy)]
 pub struct WrappingU32 {
     value: u32,
 }
@@ -8,6 +10,13 @@ pub struct WrappingU32 {
 impl WrappingU32 {
     pub fn new(value: u32) -> Self {
         Self { value }
+    }
+}
+impl Add for WrappingU32 {
+    type Output = WrappingU32;
+
+    fn add(self, other: Self) -> Self::Output {
+        Self::new(self.value + other.value)
     }
 }
 
